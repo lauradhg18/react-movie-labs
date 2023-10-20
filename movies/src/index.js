f7d434e3d69dd35bda6b32ad48f034dee30def35
lauradhg18 <lauradhg18@uma.es>
2023-10-20 18:38:49 +0200

    Added upcoming movies page

--- movies/src/index.js
+++ movies/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes} from "react-router-dom";
 import HomePage from "./pages/homePage";
+import UpcomingPage from "./pages/upcomingPage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/movies/upcoming" element={ <UpcomingPage /> } />
       </Routes>
     </BrowserRouter>
   );
