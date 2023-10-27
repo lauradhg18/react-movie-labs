01859c4bbe606d8ca4765e236d9424bd6c132165
lauradhg18 <lauradhg18@uma.es>
2023-10-27 17:16:19 +0200

    Bug Fix - make action icons on movie card configurable.

--- movies/src/pages/favoriteMoviesPage.js
+++ movies/src/pages/favoriteMoviesPage.js
@@ -4,6 +4,8 @@ import { MoviesContext } from "../contexts/moviesContext";
 import { useQueries } from "react-query";
 import { getMovie } from "../api/tmdb-api";
 import Spinner from '../components/spinner'
+import RemoveFromFavorites from "../components/cardIcons/removeFromFavorites";
+import WriteReview from "../components/cardIcons/writeReview";
 
 const FavoriteMoviesPage = () => {
   const {favorites: movieIds } = useContext(MoviesContext);
@@ -33,10 +35,17 @@ const FavoriteMoviesPage = () => {
 
   return (
     <PageTemplate
-      title="Favourite Movies"
-      movies={movies}
-      selectFavorite={toDo}
-    />
+    title="Favorite Movies"
+    movies={movies}
+    action={(movie) => {
+      return (
+        <>
+          <RemoveFromFavorites movie={movie} />
+          <WriteReview movie={movie} />
+        </>
+      );
+    }}
+  />
   );
 };
 
