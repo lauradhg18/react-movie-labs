251dd1e73272ba417fde99a914400bf5b5bce32e
lauradhg18 <lauradhg18@uma.es>
2023-10-27 16:48:59 +0200

    Using react-query to manage the browser-based cache.

--- movies/src/index.js
+++ movies/src/index.js
@@ -7,22 +7,37 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
         <SiteHeader />
-      <Routes>
-        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
-        <Route path="/movies/upcoming" element={ <UpcomingPage /> } />
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+          {/* <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} /> */}
+          <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+          <Route path="/movies/:id" element={<MoviePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={ <Navigate to="/" /> } />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
-const rootElement = createRoot( document.getElementById("root") )
+const rootElement = createRoot( document.getElementById("root") )   // no si hay que borrarlo
 rootElement.render(<App />);
