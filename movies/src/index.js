abb3e76bb71e9f8b81270e21cd0ecb7fb7e8985a
lauradhg18 <lauradhg18@uma.es>
2023-10-27 17:25:06 +0200

    Added new review support

--- movies/src/index.js
+++ movies/src/index.js
@@ -10,6 +10,7 @@ import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 
 
 const queryClient = new QueryClient({
@@ -33,6 +34,7 @@ const App = () => {
           {/* <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} /> */}
           <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
           <Route path="/movies/:id" element={<MoviePage />} />
+          <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
         </Routes>
